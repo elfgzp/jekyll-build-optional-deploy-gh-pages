2f42aaca0596114fcf5406cab5ae1fa113616828
Gzp_ <me@elfgzp.cn>
2020-10-31 14:10:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 gem install bundler:1.16.5
 bundle install
 bundle exec jekyll build
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+git config --global user.email "me@elfgzp.cn"
+git config --global user.name "elfgzp"
 git checkout --orphan gh-pages
 cp ads.txt build/
 cd build/
 touch .nojekyll
-remote_repo="https://x-access-token:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
